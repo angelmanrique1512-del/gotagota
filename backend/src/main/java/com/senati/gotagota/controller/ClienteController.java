dd05b0b748d67d2f6f5f6f060d24d9d3bad42b34
angelmanrique1512-del <angelmanrique1512@gmail.com>
2026-04-11 14:21:24 -0500

    Update ClienteController.java

--- backend/src/main/java/com/senati/gotagota/controller/ClienteController.java
+++ backend/src/main/java/com/senati/gotagota/controller/ClienteController.java
@@ -2,10 +2,8 @@ package com.senati.gotagota.controller;
 
 import com.senati.gotagota.entity.Cliente;
 import com.senati.gotagota.service.ClienteService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,6 +25,12 @@ public class ClienteController {
     //GET /api/clientes -> devuelve todos los clientes en formato JSON
     @GetMapping
     public List<Cliente> listar() {return clienteService.listarTodos();}
+    //delete /api/cliente/{id} =>eliminar
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminar(@PathVariable Long id){
+        clienteService.eliminarCliente(id);
+        return ResponseEntity.noContent().build();
+    }
 }
 
 
